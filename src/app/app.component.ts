528bcd36d6f5bc0f21db9d2f5cba62710f555813
kf <k@enemyspeak.com>
2018-01-03 16:39:49 -0600

    yep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,63 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app',
+  styleUrls: ['./app.component.css'],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  contacts: object;
+
+  constructor(private route: ActivatedRoute) {
+  }
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.contacts = [
+      {
+      "id" : "resume",
+      "name" : "Resume",
+      "icon" : "fi-page-filled",
+      "URL" : "/res/resume-faherty.pdf"
+      },
+      {
+      "id" : "email",
+      "name" : "Email",
+      "icon" : "fi-mail",
+      "URL" : "mailto:hello@enemyspeak.com"
+      },
+      {
+      "id" : "twitter",
+      "name" : "Twitter",
+      "icon" : "fi-social-twitter",
+      "URL" : "https://twitter.com/enemyspeak"
+      },
+      {
+      "id" : "tumblr",
+      "name" : "Blog",
+      "icon" : "fi-social-tumblr",
+      "URL" : "http://blog.enemyspeak.com/"
+      },
+      {
+      "id" : "art-tumblr",
+      "name" : "Art Blog",
+      "icon" : "fi-social-tumblr",
+      "URL" : "http://paint.enemyspeak.com/"
+      },
+      {
+      "id" : "instagram",
+      "name" : "Instagram",
+      "icon" : "fi-social-instagram",
+      "URL" : "https://www.instagram.com/enemyspeaks/"
+      },
+      {
+      "id" : "github",
+      "name" : "Github",
+      "icon" : "fi-social-github",
+      "URL" : "https://github.com/enemyspeak"
+      }
+    ]
+    });
+  }
 }
