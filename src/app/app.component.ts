d612f71f4b6cf9d13c75ea7a78eaa3158eebf2f6
kf <k@enemyspeak.com>
2018-01-04 00:19:53 -0600

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import { Contact } from './contact';
-import { CONTACTS } from './contacts';
+import { Contact,CONTACTS } from './contact';
 
 @Component({
   selector: 'app',
--- src/app/contact.ts
+++ src/app/contact.ts
@@ -4,3 +4,48 @@ export class Contact {
   icon: string;
   URL: string;
 }
+
+export const CONTACTS: Contact[] = [
+      {
+      "id" : "resume",
+      "name" : "Resume",
+      "icon" : "fi-page-filled",
+      "URL" : "/res/resume-faherty.pdf"
+      },
+      {
+      "id" : "email",
+      "name" : "Email",
+      "icon" : "fi-mail",
+      "URL" : "mailto:hello@enemyspeak.com"
+      },
+      {
+      "id" : "twitter",
+      "name" : "Twitter",
+      "icon" : "fi-social-twitter",
+      "URL" : "https://twitter.com/enemyspeak"
+      },
+      {
+      "id" : "tumblr",
+      "name" : "Blog",
+      "icon" : "fi-social-tumblr",
+      "URL" : "http://blog.enemyspeak.com/"
+      },
+      {
+      "id" : "art-tumblr",
+      "name" : "Art Blog",
+      "icon" : "fi-social-tumblr",
+      "URL" : "http://paint.enemyspeak.com/"
+      },
+      {
+      "id" : "instagram",
+      "name" : "Instagram",
+      "icon" : "fi-social-instagram",
+      "URL" : "https://www.instagram.com/enemyspeaks/"
+      },
+      {
+      "id" : "github",
+      "name" : "Github",
+      "icon" : "fi-social-github",
+      "URL" : "https://github.com/enemyspeak"
+      }
+    ];
